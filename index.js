3ed8234b326a3caefced57f6c82a4cc85c11978a
Abhishek kumar <abhishekkumar@Abhisheks-MacBook-Air-2.local>
2024-06-20 15:31:16 +0530

    ejs page added

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs")
+
+
 app.get('/', (req, res) => {
     res.send('Hello World! deployed')
 })
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
